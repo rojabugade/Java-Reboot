c4ad0c6b479fe81e18949909bbaa3c96d44dfd47
rojabugade <rojabugade@gmail.com>
2025-09-07 20:29:16 -0400

    updating model class

--- todoapp/todoapp/src/main/java/com/example/todoapp/model/Todo.java
+++ todoapp/todoapp/src/main/java/com/example/todoapp/model/Todo.java
@@ -12,13 +12,31 @@ public class Todo {
     private String title;
     private boolean completed;
 
-    public Todo() {} // No-args constructor
+    public Todo() {
+    } // No-args constructor
 
     // Getters & Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-    public boolean isCompleted() { return completed; }
-    public void setCompleted(boolean completed) { this.completed = completed; }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
